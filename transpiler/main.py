b9d68621ee498e5344f55187fab0c6ad939c6e31
Josh Gruenstein <joshuagruenstein@users.noreply.github.com>
2018-08-08 15:23:28 -0700

    Update Java transpiler api to accept multiple sources

--- transpiler/main.py
+++ transpiler/main.py
@@ -28,31 +28,42 @@ def handle_invalid_usage(error):
 
 @app.route("/", methods=['GET'])
 def index():
-	return "Hello World!"
+    return "Hello World!"
 
 
 @app.route("/compile", methods=['POST'])
 def compile():
-	args = request.get_json()
+    args = request.get_json()
 
-	if not 'src' in args:
-		raise InvalidUsage("Must provide a source.", status_code=422)
-	if not 'lang' in args:
-		raise InvalidUsage("Must specify compilation language.", status_code=422)
+    if not 'src' in args:
+        raise InvalidUsage("Must provide a source.", status_code=422)
+    if not 'lang' in args:
+        raise InvalidUsage("Must specify compilation language.", status_code=422)
 
-	if args['lang'] == 'java':
-		return jsonify(java.compile(str(args['src'])))
+    # Intake a list of files rather than a source for Java
+    if args['lang'] == 'java':
+        if isinstance(args['src'], (list,)):
+            for src in args['src]:
+                if isinstance(src, dict) and 'source' in src and 'filename' in src:
+                    src['source'] = str(src['source'])
+                    src['filename'] = str(src['filename']).split('/')[0]
+                else:
+                    raise InvalidUsage("Must provide source and filename for Java files.")
+        else:
+            raise InvalidUsage("Must provide list of JSON sources for Java target.", status_code=422)
+        
+	return jsonify(java.compile(args['src']))
 
-	elif args['lang'] == 'js':
-		return jsonify(js.compile(str(args['src'])))
+    elif args['lang'] == 'js':
+        return jsonify(js.compile(str(args['src'])))
 
-	elif args['lang'] == 'python':
-		return jsonify(python.compile(str(args['src'])))
+    elif args['lang'] == 'python':
+        return jsonify(python.compile(str(args['src'])))
 
-	else:
-		raise InvalidUsage("Invalid language specified.", status_code=400)
+    else:
+        raise InvalidUsage("Invalid language specified.", status_code=400)
 
 
 if __name__ == "__main__":
-	# Only for debugging while developing
-	app.run(host='0.0.0.0', debug=False, port=8080)
+    # Only for debugging while developing
+    app.run(host='0.0.0.0', debug=False, port=8080)
