28f9a31be57d4be1449a8417d421be36e4593100
joshuagruenstein <joshuagruenstein@gmail.com>
2018-08-12 13:29:17 -0700

    CORS for transpiler

--- transpiler/main.py
+++ transpiler/main.py
@@ -1,7 +1,9 @@
 from flask import Flask, request, jsonify
+from flask_cors import CORS
 import java, python
 
 app = Flask(__name__)
+CORS(app)
 
 class InvalidUsage(Exception):
     status_code = 400
