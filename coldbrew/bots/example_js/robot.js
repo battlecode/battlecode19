849368670cd4cbcdc6090ec02f324ab802a606d7
Kelvin Lu <kelvinlu@mit.edu>
2019-01-07 18:30:24 -0500

    Example Bots

---
+++ coldbrew/bots/example_js/robot.js
@@ -0,0 +1,29 @@
+import {BCAbstractRobot, SPECS} from 'battlecode';
+
+var built = false;
+var step = -1;
+
+class MyRobot extends BCAbstractRobot {
+    turn() {
+        step++;
+
+        if (this.me.unit === SPECS.CRUSADER) {
+            // this.log("Crusader health: " + this.me.health);
+            const choices = [[0,-1], [1, -1], [1, 0], [1, 1], [0, 1], [-1, 1], [-1, 0], [-1, -1]];
+            const choice = choices[Math.floor(Math.random()*choices.length)]
+            return this.move(...choice);
+        }
+
+        else if (this.me.unit === SPECS.CASTLE) {
+            if (step % 10 === 0) {
+                this.log("Building a crusader at " + (this.me.x+1) + ", " + (this.me.y+1));
+                return this.buildUnit(SPECS.CRUSADER, 1, 1);
+            } else {
+                return // this.log("Castle health: " + this.me.health);
+            }
+        }
+
+    }
+}
+
+var robot = new MyRobot();
