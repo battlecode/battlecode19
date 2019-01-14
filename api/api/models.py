78969f6b9010846a2aea7ffc30b323152867450b
Joshua Segaran <jsegaran1@gmail.com>
2019-01-14 18:00:20 -0500

    Reverse name overiding

--- api/api/models.py
+++ api/api/models.py
@@ -13,7 +13,7 @@ from django.db import models
 from django.db.models.signals import pre_save, post_save
 from django.dispatch import receiver
 from django.template.loader import render_to_string
-from django.urls import reverse
+# from django.urls import reverse
 from django_rest_passwordreset.signals import reset_password_token_created
 
 
