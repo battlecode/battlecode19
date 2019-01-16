869e784ff9cc92c66fb423b497477fed9bc2cb61
Joshua Segaran <jsegaran1@gmail.com>
2019-01-15 19:31:30 -0500

    Add debug

--- api/settings.py
+++ api/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.getenv('SECRET', 'asdf')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = os.getenv('DEBUG', False)
 
 ALLOWED_HOSTS = ['*']
 
